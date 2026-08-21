a45f2dc0f4c766a596280efbf43eca436db1eaa2
emrebarisc <22220055+emrebarisc@users.noreply.github.com>
2026-08-21 15:25:42 +0300

    Set mesh viewer camera near distance to 0.0001

--- Source/Objects/MeshViewerCameraObject.cpp
+++ Source/Objects/MeshViewerCameraObject.cpp
@@ -1,11 +1,15 @@
 #include "MeshViewerCameraObject.h"
+
+#include "Goknar/Camera.h"
 #include "Goknar/Components/CameraComponent.h"
+
 #include "Controllers/MeshViewerCameraController.h"
 
 MeshViewerCameraObject::MeshViewerCameraObject() : ObjectBase()
 {
 	cameraComponent_ = AddSubComponent<CameraComponent>();
 	cameraComponent_->SetCameraFollowsComponentRotation(true);
+	cameraComponent_->GetCamera()->SetNearDistance(0.0001f);
 	SetRootComponent(cameraComponent_);
 
 	controller_ = new MeshViewerCameraController(this);
